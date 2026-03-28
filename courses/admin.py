2dcb50e2edca0fc4bb3b846fc6130bb767bebfb9
gabriel <gabriel@roverpix.com>
2026-03-27 22:28:28 -0300

    The first config (And simple) of admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+@admin.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'duration', 'price', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['name', 'description']
+    list_editable = ['is_active']
+    list_per_page = 10
+    ordering = ['name']
+    readonly_fields = ['created_at', 'updated_at']
+    fields = ['name', 'description', 'duration', 'price', 'is_active', 'created_at', 'updated_at']
