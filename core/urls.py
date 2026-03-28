c96fdcc8d6ed0db5f85a71d305663b3db265ee48
gabriel <gabriel@roverpix.com>
2026-03-27 22:27:46 -0300

    Making config of admin and changing their with jazzmin

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+admin.site.site_header = "Portal Macuco"
+admin.site.site_title = "Portal Bioparque Macuco"
+admin.site.index_title = "MacucoAdmin"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
