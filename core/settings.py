5cd3dbc899b7069fd44fff5c51db6a2af6484a7e
Gabriel Limone Nunes <glimone.nunes@outlook.com>
2026-04-01 22:19:53 -0300

    Revert "Revert "feat: update models and Course fields and Added core models: Class, Student, Enrollment""

--- core/settings.py
+++ core/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': 'localhost',
-        'PORT': '5433',
+        'PORT': '5432',
     }
 }
 
@@ -134,3 +134,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+print(os.getenv('POSTGRES_DB'))
