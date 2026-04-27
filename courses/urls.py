8372c097f86f4fabe9640f1ce7ce0b97e41e2efe
gabriel <gabriel@roverpix.com>
2026-04-27 10:00:22 -0300

    Adding basename to course url

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import CourseViewSet
 
 router = DefaultRouter()
-router.register(r'courses', CourseViewSet)
+router.register(r'courses', CourseViewSet, basename='course')
 
 urlpatterns = router.urls
